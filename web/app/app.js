234df91b2892cd4861d63432855ce8bbe2c3207d
Andrew Valencik <mightyhops@gmail.com>
2014-03-21 23:12:19 -0300

    Added initial web app. Displays KEYNO from mongodb.

---
+++ web/app/app.js
@@ -0,0 +1,49 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var NSReferenceProvider = require('./dbHandler.js').NSReferenceProvider;
+
+// Express environment setup
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//Setup the mongo handler
+var NSReferenceGrab = new NSReferenceProvider('127.0.0.1', 27017);
+
+// Routes 
+app.get('/', function(req, res){
+  NSReferenceGrab.findStuff({}, 20, function(error, docs){
+    console.log('##findStuff:  ', docs[0]);
+    res.render('index', {
+      title: 'Papers',
+      papers: docs
+    });
+  });
+});
+//app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
