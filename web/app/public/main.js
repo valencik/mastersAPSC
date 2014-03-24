3df9bce4e194c938430115e65c1516d1f3a19e1b
Andrew Valencik <mightyhops@gmail.com>
2014-03-24 03:58:26 -0300

    Charting is more modular. Added document type info and pie charts.

--- web/app/public/main.js
+++ web/app/public/main.js
@@ -15,9 +15,7 @@ $(document).ready(function() {
         });
     };
 
-    var graph = function(selector, title, collection, pipeline, options) {
-
-        // Discrete Bar graph
+    var graph = function(selector, title, collection, pipeline, options, graphType, callback) {
         aggregate(collection, 
             pipeline,
             options,
@@ -27,48 +25,152 @@ $(document).ready(function() {
                     values: results
                 }];
                 console.log(data);
+                
+                switch(graphType){
+                    case 'discreteBarChart':
+                        nv.addGraph(discreteBarChart(selector, title, data, callback));
+                        console.log("Graphing discreteBarChart")
+                        break;
+                    case 'pieChart':
+                        nv.addGraph(pieChart(selector, title, data, callback));
+                        console.log("Graphing pieChart")
+                        break;
+                    default:
+                        console.log("# No valid graph type detected.")
+                }
 
-                nv.addGraph(function() {
-                  var chart = nv.models.discreteBarChart()
-                      .x(function(d) { return d.label })    //Specify the data accessors.
-                      .y(function(d) { return d.value })
-                      .staggerLabels(false)    //Too many bars and not enough room? Try staggering labels.
-                      .tooltips(true)        //Don't show tooltips
-                      .showValues(false)       //...instead, show the bar value right on top of each bar.
-                      .transitionDuration(350)
-                      ;
-
-                      chart.yAxis.tickFormat(d3.format('.0f'));
-                      chart.xAxis.tickValues([1899,1910,1920,1930,1940,1950,1960,1970,1980,1990,2000,2010]);
-
-                  d3.select(selector)
-                      .datum(data)
-                      .call(chart);
-
-                  nv.utils.windowResize(chart.update);
-
-                  return chart;
-                });
-        });
-
+            });
     };
 
+//
+//   GRAPH MODELS
+//
+
+    //Define discreteBarChart
+    discreteBarChart = function(selector, title, data, callback) {
+        var chart = nv.models.discreteBarChart()
+            .x(function(d) { return d.label })    //Specify the data accessors.
+            .y(function(d) { return d.value })
+            .staggerLabels(false)
+            .tooltips(true) 
+            .showValues(false)
+            .transitionDuration(350)
+            ;
+
+        callback && callback(chart);
+        d3.select(selector)
+            .datum(data)
+            .call(chart);
+
+        nv.utils.windowResize(chart.update);
+        return chart;
+      };
+
+    //Define pieChart
+    pieChart = function(selector, title, data, callback) {
+        var chart = nv.models.pieChart()
+            .x(function(d) { return d.label })
+            .y(function(d) { return d.value })
+            .showLabels(true);
+
+        callback && callback(chart, data);
+        d3.select(selector)
+            .datum(data[0].values)
+            .transition().duration(350)
+            .call(chart);
+
+        return chart;
+      };
+
+
+//
+//   GRAPH VIEWS
+//
+
     // Graph yearly summary
-    var years = [];
-    for(i=1896; i<=2014; i++) { years.push(i.toString()) }
+    graphType = "discreteBarChart";
     graph(
         "#charts svg#yearly", 
         "NSR Data", 
         "NSR", 
         [
-            { $match: { YEAR: { $in: years } } }, 
-            { $group: { _id: "$YEAR", total: { $sum: 1 } } }, 
-            { $sort: { _id: 1 } },
-            { $project : {_id : 0, label : "$_id", value : "$total"} },
+            { $group: { _id: "$keyNumber.yearPublication", total: { $sum: 1} } },
+            { $sort: {_id: 1} },
+            {$project: {_id: 0, label: "$_id", value: "$total" } } 
         ],
-        {}
+        {},
+        graphType,
+        function(chart){
+            chart.yAxis.tickFormat(d3.format('.0f'));
+            chart.xAxis.tickValues([1899,1910,1920,1930,1940,1950,1960,1970,1980,1990,2000,2010]);
+        }
     );
 
+    // Graph 90 year summary
+    graphType = "discreteBarChart";
+    graph(
+        "#charts svg#years90", 
+        "NSR Data", 
+        "NSR", 
+        [
+            { $match: {"keyNumber.yearPublication": {$gt: 1930}}},
+            { $group: { _id: "$keyNumber.yearPublication", total: { $sum: 1} } },
+            { $sort: {_id: 1} },
+            {$project: {_id: 0, label: "$_id", value: "$total" } } 
+        ],
+        {},
+        graphType,
+        function(chart){
+            chart.yAxis.tickFormat(d3.format('.0f'));
+            chart.xAxis.tickValues([1930,1940,1950,1960,1970,1980,1990,2000,2010]);
+        }
+    );
+
+    // Pie chart 1
+    graphType = "discreteBarChart";
+    types = ["JOUR", "CONF", "REPT", "BOOK", "PC", "THESIS", "PREPRINT"];
+    graph(
+        "#charts svg#doctypes", 
+        "NSR Data", 
+        "NSR2", 
+        [
+             { $match: { "CODEN.0": { $in: types } } },
+             { $unwind: "$CODEN" },
+             { $match: { "CODEN": { $in: types } } },
+             { $group: { _id: "$CODEN", total: { $sum: 1 } }},
+             { $project : {_id : 0, label : "$_id", value : "$total"} },
+             { $sort: {value: -1} }
+        ],
+        {},
+        graphType,
+        function(chart, data){console.log("Printed Bar Chart of doctypes: "+data);}
+    );
+
+    // Pie chart 1
+    graphType = "pieChart";
+    types = ["JOUR", "CONF", "REPT", "BOOK", "PC", "THESIS", "PREPRINT"];
+    graph(
+        "#charts svg#doctypes2", 
+        "NSR Data", 
+        "NSR2", 
+        [
+             { $match: { "CODEN.0": { $in: types } } },
+             { $unwind: "$CODEN" },
+             { $match: { "CODEN": { $in: types } } },
+             { $group: { _id: "$CODEN", total: { $sum: 1 } }},
+             { $project : {_id : 0, label : "$_id", value : "$total"} },
+             { $sort: {value: -1} }
+        ],
+        {},
+        graphType,
+        function(chart, data){
+            console.log("pie pie pie: "+data);
+            chart.labelThreshold(.01)
+            .donut(true)
+            .donutLabelsOutside(true)
+            .donutRatio(0.2);
+        }
+    );
 
 
 });
