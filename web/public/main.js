899d5d66107ea0d1eab1400cbc2b4d239e184c4a
Andrew Valencik <mightyhops@gmail.com>
2014-05-22 07:11:04 -0300

    Modified queries to new schema. Improved doctype query.

--- web/public/main.js
+++ web/public/main.js
@@ -24,16 +24,18 @@ $(document).ready(function() {
                     key: title,
                     values: results
                 }];
+                console.log("Graph: %s", graphType);
                 console.log(data);
                 
                 switch(graphType){
                     case 'discreteBarChart':
                         nv.addGraph(discreteBarChart(selector, title, data, callback));
-                        console.log("Graphing discreteBarChart")
                         break;
                     case 'pieChart':
                         nv.addGraph(pieChart(selector, title, data, callback));
-                        console.log("Graphing pieChart")
+                        break;
+                    case 'lineWithFocusChart':
+                        nv.addGraph(lineWithFocusChart(selector, title, data, callback));
                         break;
                     default:
                         console.log("# No valid graph type detected.")
@@ -82,6 +84,43 @@ $(document).ready(function() {
         return chart;
       };
 
+    //Define lineWithFocusChart
+    lineWithFocusChart = function(selector, title, data, callback) {
+        var chart = nv.models.lineWithFocusChart();
+
+        callback && callback(chart, data);
+        d3.select(selector).enter().append()
+            .datum(data)
+            .transition().duration(500)
+            .call(chart);
+
+        nv.utils.windowResize(chart.update);
+        return chart;
+      };
+
+    multiBarChart = function(selector, title, data, callback) {
+        var chart = nv.models.multiBarChart()
+          .transitionDuration(350)
+          .reduceXTicks(true)   //If 'false', every single x-axis tick label will be rendered.
+          .rotateLabels(0)      //Angle to rotate x-axis labels.
+          .showControls(true)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
+          .groupSpacing(0.1)    //Distance between each group of bars.
+        ;
+    
+        chart.xAxis
+            .tickFormat(d3.format(',f'));
+    
+        chart.yAxis
+            .tickFormat(d3.format(',.1f'));
+    
+        d3.select('#chart1 svg')
+            .datum(data)
+            .call(chart);
+    
+        nv.utils.windowResize(chart.update);
+        return chart;
+    };
+
 
 //
 //   GRAPH VIEWS
@@ -94,7 +133,7 @@ $(document).ready(function() {
         "NSR Data", 
         "NSR3", 
         [
-            { $group: { _id: "$keyNumber.yearPublication", total: { $sum: 1} } },
+            { $group: { _id: "$year", total: { $sum: 1} } },
             { $sort: {_id: 1} },
             {$project: {_id: 0, label: "$_id", value: "$total" } } 
         ],
@@ -108,43 +147,43 @@ $(document).ready(function() {
 
     // Doctype Bar Chart
     graphType = "discreteBarChart";
-    types = ["JOUR", "CONF", "REPT", "BOOK", "PC", "THESIS", "PREPRINT"];
     graph(
         "#charts svg#doctypes", 
         "NSR Data", 
         "NSR3", 
         [
-             { $match: { "CODEN.0": { $in: types } } },
-             { $unwind: "$CODEN" },
-             { $match: { "CODEN": { $in: types } } },
-             { $group: { _id: "$CODEN", total: { $sum: 1 } }},
-             { $project : {_id : 0, label : "$_id", value : "$total"} },
-             { $sort: {value: -1} }
-        ],
-        {},
-        graphType,
-        function(chart, data){console.log("Printed Bar Chart of doctypes: "+data);}
-    );
-
-    // Doctype Pie
-    graphType = "pieChart";
-    types = ["JOUR", "CONF", "REPT", "BOOK", "PC", "THESIS", "PREPRINT"];
-    graph(
-        "#charts svg#doctypes2", 
-        "NSR Data", 
-        "NSR3", 
-        [
-             { $match: { "CODEN.0": { $in: types } } },
-             { $unwind: "$CODEN" },
-             { $match: { "CODEN": { $in: types } } },
-             { $group: { _id: "$CODEN", total: { $sum: 1 } }},
+             { $group: { _id: "$type", total: { $sum: 1} } },
+             { $sort: {total: -1}},
+             { $limit: 8},
+             { $sort: {_id: -1}},
+             { $limit: 7},
              { $project : {_id : 0, label : "$_id", value : "$total"} },
              { $sort: {value: -1} }
         ],
         {},
         graphType,
         function(chart, data){
-            console.log("pie pie pie: "+data);
+        }
+    );
+
+    // Doctype Pie
+    graphType = "pieChart";
+    graph(
+        "#charts svg#doctypes2", 
+        "NSR Data", 
+        "NSR3", 
+        [
+             { $group: { _id: "$type", total: { $sum: 1} } },
+             { $sort: {total: -1}},
+             { $limit: 8},
+             { $sort: {_id: -1}},
+             { $limit: 7},
+             { $project : {_id : 0, label : "$_id", value : "$total"} },
+             { $sort: {value: -1} }
+        ],
+        {},
+        graphType,
+        function(chart, data){
             chart.labelThreshold(.01)
             .donut(true).donutLabelsOutside(true).donutRatio(0.2);
         }
@@ -155,7 +194,7 @@ $(document).ready(function() {
 for (i=1950; i<=1970; i=i+10) {
     graph("#charts svg#prolific"+(i-1900), "NSR Data", "NSR3", 
         [
-             { $match: {"keyNumber.yearPublication": {$gt: i, $lte: i+10}}},
+             { $match: {"year": {$gt: i, $lte: i+10}}},
              { $unwind: "$authors"},
              { $group: { _id: "$authors", total: { $sum: 1} } },
              { $match: {total: {$gt: i-1925} } },
@@ -177,4 +216,57 @@ for (i=1950; i<=1970; i=i+10) {
 }
 
 
-});
+    // Yearly papers with View Finder
+    graphType = "lineWithFocusChart";
+    types = ["JOUR", "CONF", "REPT", "BOOK", "PC", "THESIS", "PREPRINT"];
+    types.forEach(function(i){graph(
+        "#charts svg#yearlyFinder", 
+        i, 
+        "NSR3", 
+        [
+            { $match: {"type": i}},
+            { $group: { _id: "$year", total: { $sum: 1} } },
+            { $sort: {_id: 1} },
+            { $project: {_id: 0, x: "$_id", y: "$total" } } 
+        ],
+        {},
+        graphType,
+        function(chart){
+            chart.xAxis
+              .tickFormat(d3.format(',f'));
+          
+            chart.yAxis
+              .tickFormat(d3.format(',.2f'));
+          
+            chart.y2Axis
+              .tickFormat(d3.format(',.2f'));
+            //chart.yAxis.tickFormat(d3.format('.0f'));
+            //chart.xAxis.tickValues([1899,1910,1920,1930,1940,1950,1960,1970,1980,1990,2000,2010]);
+        }
+    )}
+);
+
+
+    // Multi Bar Chart
+    graphType = "multiBarChart";
+    graph(
+        "#charts svg#multi", 
+        "NSR Data", 
+        "NSR3", 
+        [
+            {$group: { _id: {type: "$type", year: "$year"}, total: { $sum: 1} } },
+            {$sort: {"sum": -1}},
+            {$project: {_id: 0, type: "$_id.type", x: "$_id.year", y: "$total"}},
+            {$group: {_id: "$type", values: {$addToSet: {x: "$x", y: "$y"}}}},
+            {$project: {_id:0, key: "$_id", values: "$values"}}
+       ],
+        {},
+        graphType,
+        function(chart, data){
+        }
+    );
+
+
+
+
+}); //Thats all folks
