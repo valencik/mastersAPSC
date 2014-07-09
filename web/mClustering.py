ff897d55cdfbe8a59389ce5bf27aa5e24149db0e
Andrew Valencik <mightyhops@gmail.com>
2014-07-08 21:48:51 -0300

    Completed clustering prototype. Output questionable.

--- web/mClustering.py
+++ web/mClustering.py
@@ -1,15 +1,28 @@
 import numpy
 import pymongo
+import pylab as pl
 
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.decomposition import PCA
+
+#Connect to MongoDB and get data to cluster
 c = pymongo.Connection("localhost")
-cluster = c.masters.cluster1
-num = cluster.count()
-arrays = numpy.empty((num, len(headers)), dtype=int) 
+collection = c.masters.cluster1
 
-for i, record in enumerate(cluster.find()):
-    for x in range(len(headers)):
-        arrays[i][x] = record[headers[x]]
+#The find filter should be as follows:
+#{"type": 1, "physics": 1, "reactions": 1, "_id": 0, "moments": 1, "year": 1, "masses": 1, "structure": 1, "compilation": 1, "radioactivity": 1}
+clusterCursor = collection.find({},{"type": 1, "physics": 1, "reactions": 1, "_id": 0, "moments": 1, "masses": 1, "structure": 1, "compilation": 1, "radioactivity": 1})
 
-for array in arrays: # prove that we did something...
-    print(numpy.mean(array))
+#Feature Extraction with sklearn
+#http://scikit-learn.org/stable/modules/feature_extraction.html
+vec = DictVectorizer(dtype=int)
 
+cdata = vec.fit_transform(clusterCursor).toarray()
+
+#decompisition
+pca = PCA(n_components=2)
+pca.fit(cdata)
+x = pca.transform(cdata)
+pl.scatter(x[:, 0], x[:, 1])
+
+pl.show()
