1afd0fe6d922aa058ced7b26a3acbc819b0ed4ae
Andrew Valencik <mightyhops@gmail.com>
2014-07-10 10:51:40 -0300

    Added kmeans. Variable renaming for clarity.

--- web/mClustering.py
+++ web/mClustering.py
@@ -1,9 +1,11 @@
+#Should improve imports for speed
 import numpy
 import pymongo
 import pylab as pl
 
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.decomposition import PCA
+from sklearn import cluster
 
 #Connect to MongoDB and get data to cluster
 c = pymongo.Connection("localhost")
@@ -13,16 +15,21 @@ collection = c.masters.cluster1
 #{"type": 1, "physics": 1, "reactions": 1, "_id": 0, "moments": 1, "year": 1, "masses": 1, "structure": 1, "compilation": 1, "radioactivity": 1}
 clusterCursor = collection.find({},{"type": 1, "physics": 1, "reactions": 1, "_id": 0, "moments": 1, "masses": 1, "structure": 1, "compilation": 1, "radioactivity": 1})
 
-#Feature Extraction with sklearn
+#Feature Extraction with sklearn on MongoDB cursor
 #http://scikit-learn.org/stable/modules/feature_extraction.html
+#vec.fit_transform returns sparse data, we use toarray() to convert to dense.
 vec = DictVectorizer(dtype=int)
+denseData = vec.fit_transform(clusterCursor).toarray()
 
-cdata = vec.fit_transform(clusterCursor).toarray()
-
-#decompisition
+#Decompisition
 pca = PCA(n_components=2)
-pca.fit(cdata)
-x = pca.transform(cdata)
-pl.scatter(x[:, 0], x[:, 1])
+pca.fit(denseData)
+reducedData = pca.transform(denseData)
 
+#Clustering
+k_means = cluster.KMeans(n_clusters=3)
+k_means.fit(denseData)
+
+#Plotting
+pl.scatter(reducedData[:, 0], reducedData[:, 1], c=k_means.predict(denseData))
 pl.show()
