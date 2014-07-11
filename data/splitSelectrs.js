c72d635d08679c0c548f46cc6c0ddf6ea224c6f1
Andrew Valencik <mightyhops@gmail.com>
2014-07-11 11:20:06 -0300

    Added mongoDB commands to flatten data.
    Rearranged commands. Added comments.
    Removed old code example snippets.

--- data/splitSelectrs.js
+++ data/splitSelectrs.js
@@ -1,3 +1,36 @@
+//Make collection for flattening.
+db.NSR3.aggregate(
+   {$match: {year: 1971}},
+   {$project: {year: 1, type: 1, authors: 1, reactions: 1, structure: 1, radioactivity: 1, physics: 1, moments: 1, compilation: 1, masses: 1}},
+   {$out: "cluster"}
+)
+
+//Flatten data for clustering in python
+db.cluster.find({}).forEach(function (x) {
+    switch(x["type"]){
+        case 'JOUR': x["type"] = 1; break;
+        case 'CONF': x["type"] = 2; break;
+        case 'REPT': x["type"] = 3; break;
+        case 'BOOK': x["type"] = 4; break;
+        case 'PC': x["type"] = 5; break;
+        case 'THESIS': x["type"] = 6; break;
+        case 'PREPRINT': x["type"] = 7; break;
+        default: x["type"] = 8;
+    }
+
+    x["authors"] = x["authors"].length;
+
+    (typeof x["reactions"] != 'undefined') ? x["reactions"]=1 : x["reactions"]=0;
+    (typeof x["structure"] != 'undefined') ? x["structure"]=1 : x["structure"]=0;
+    (typeof x["radioactivity"] != 'undefined') ? x["radioactivity"]=1 : x["radioactivity"]=0;
+    (typeof x["physics"] != 'undefined') ? x["physics"]=1 : x["physics"]=0;
+    (typeof x["moments"] != 'undefined') ? x["moments"]=1 : x["moments"]=0;
+    (typeof x["compilation"] != 'undefined') ? x["compilation"]=1 : x["compilation"]=0;
+    (typeof x["masses"] != 'undefined') ? x["masses"]=1 : x["masses"]=0;
+    db.cluster1.save(x);
+});
+
+//Split SELECTRS into paramType, paramValue, and linkVar
 db.NSR2.find({}).forEach(function (x) {
     var old = x["SELECTRS"] || "";
     var s = old.split('.');
@@ -20,56 +53,3 @@ db.NSR2.find({}).forEach(function (x) {
     // save to a new one
     db.NSR3.save(x);
 });
-
-db.cluster.find({}).forEach(function (x) {
-    switch(x["type"]){
-        case 'JOUR':
-            x["type"] = 1
-            break;
-        case 'CONF':
-            x["type"] = 2
-            break;
-        case 'REPT':
-            x["type"] = 3
-            break;
-        case 'BOOK':
-            x["type"] = 4
-            break;
-        case 'PC':
-            x["type"] = 5
-            break;
-        case 'THESIS':
-            x["type"] = 6
-            break;
-        case 'PREPRINT':
-            x["type"] = 7
-            break;
-        default:
-            x["type"] = 8
-    }
-
-    (typeof x["reactions"] != 'undefined') ? x["reactions"]=1 : x["reactions"]=0;
-    (typeof x["structure"] != 'undefined') ? x["structure"]=1 : x["structure"]=0;
-    (typeof x["radioactivity"] != 'undefined') ? x["radioactivity"]=1 : x["radioactivity"]=0;
-    (typeof x["physics"] != 'undefined') ? x["physics"]=1 : x["physics"]=0;
-    (typeof x["moments"] != 'undefined') ? x["moments"]=1 : x["moments"]=0;
-    (typeof x["compilation"] != 'undefined') ? x["compilation"]=1 : x["compilation"]=0;
-    (typeof x["masses"] != 'undefined') ? x["masses"]=1 : x["masses"]=0;
-    db.cluster1.save(x);
-});
-
-                switch(queryType.graph){
-                    case 'discreteBarChart':
-                        nv.addGraph(discreteBarChart(selector, title, data, callback));
-                        break;
-                    case 'multiBarChart':
-                        nv.addGraph(multiBarChart(selector, title, data, callback));
-                        break;
-                    case 'pieChart':
-                        nv.addGraph(pieChart(selector, title, data, callback));
-                        break;
-                    case 'lineWithFocusChart':
-                        nv.addGraph(lineWithFocusChart(selector, title, data, callback));
-                        break;
-                    default:
-                        console.log("# No valid graph type detected.")
