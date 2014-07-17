dd32643fc5b754e4639bf53f6b6c470d6c8f7f0e
Andrew Valencik <mightyhops@gmail.com>
2014-07-17 10:22:53 -0300

    Added some options, commented out others.

--- web/mClustering.py
+++ web/mClustering.py
@@ -9,16 +9,19 @@ from sklearn import cluster
 
 #Connect to MongoDB and get data to cluster
 c = pymongo.Connection("localhost")
-collection = c.masters.cluster1
+#collection = c.masters.j11cluster
+#collection = c.masters.j11clusterB
+collection = c.masters.j11clusterNorm
 
 #The find filter should be as follows:
 #{"type": 1, "physics": 1, "reactions": 1, "_id": 0, "moments": 1, "year": 1, "masses": 1, "structure": 1, "compilation": 1, "radioactivity": 1}
-clusterCursor = collection.find({},{"type": 1, "physics": 1, "reactions": 1, "_id": 0, "moments": 1, "masses": 1, "structure": 1, "compilation": 1, "radioactivity": 1})
+clusterCursor = collection.find({},{"_id": 0})
 
 #Feature Extraction with sklearn on MongoDB cursor
 #http://scikit-learn.org/stable/modules/feature_extraction.html
 #vec.fit_transform returns sparse data, we use toarray() to convert to dense.
-vec = DictVectorizer(dtype=int)
+#vec = DictVectorizer(dtype=int)
+vec = DictVectorizer()
 denseData = vec.fit_transform(clusterCursor).toarray()
 
 #Decompisition
@@ -31,5 +34,11 @@ k_means = cluster.KMeans(n_clusters=3)
 k_means.fit(denseData)
 
 #Plotting
+
+##Plot clusters in reduced dimensionality
 pl.scatter(reducedData[:, 0], reducedData[:, 1], c=k_means.predict(denseData))
+
+#Plot number of authors per year
+#pl.scatter(denseData[:, 9], denseData[:, 0], c=k_means.predict(denseData))
+
 pl.show()
