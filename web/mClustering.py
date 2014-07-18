a6acc177a175caaf307f0790e15d69d4bc3f4ddd
Andrew Valencik <mightyhops@gmail.com>
2014-07-18 10:00:42 -0300

    Added coincidence prototype. Removed PCA.

--- web/mClustering.py
+++ web/mClustering.py
@@ -1,4 +1,4 @@
-#Should improve imports for speed
+#TODO Import only the minimum modules necessary
 import numpy
 import pymongo
 import pylab as pl
@@ -9,12 +9,7 @@ from sklearn import cluster
 
 #Connect to MongoDB and get data to cluster
 c = pymongo.Connection("localhost")
-#collection = c.masters.j11cluster
-#collection = c.masters.j11clusterB
 collection = c.masters.j11clusterNorm
-
-#The find filter should be as follows:
-#{"type": 1, "physics": 1, "reactions": 1, "_id": 0, "moments": 1, "year": 1, "masses": 1, "structure": 1, "compilation": 1, "radioactivity": 1}
 clusterCursor = collection.find({},{"_id": 0})
 
 #Feature Extraction with sklearn on MongoDB cursor
@@ -24,10 +19,14 @@ clusterCursor = collection.find({},{"_id": 0})
 vec = DictVectorizer()
 denseData = vec.fit_transform(clusterCursor).toarray()
 
-#Decompisition
-pca = PCA(n_components=2)
-pca.fit(denseData)
-reducedData = pca.transform(denseData)
+#Output some data attributes
+print("Retrieved", denseData.shape[0], "elements.")
+print("Data has", denseData.shape[1], "dimensions")
+
+##Decompisition
+#pca = PCA(n_components=2)
+#pca.fit(denseData)
+#reducedData = pca.transform(denseData)
 
 #Clustering
 k_means = cluster.KMeans(n_clusters=3)
@@ -36,9 +35,35 @@ k_means.fit(denseData)
 #Plotting
 
 ##Plot clusters in reduced dimensionality
-pl.scatter(reducedData[:, 0], reducedData[:, 1], c=k_means.predict(denseData))
+#pl.scatter(reducedData[:, 0], reducedData[:, 1], c=k_means.predict(denseData))
 
-#Plot number of authors per year
+##Plot number of authors per year
 #pl.scatter(denseData[:, 9], denseData[:, 0], c=k_means.predict(denseData))
+#pl.show()
 
+
+#coincidence testing
+cmat = numpy.array([[0, 0, 0, 1], [0, 0, 1, 0], [0, 0, 1, 1], [0, 1, 0, 0], [0, 1, 0, 1]])
+cdim = numpy.shape(cmat)[1]
+print("Making coincidence matrix with", cdim, "rows")
+coinMat = numpy.zeros(shape=(cdim,cdim), dtype=numpy.int)
+
+for row in cmat:
+    if row.sum() == 1:
+        i1 = numpy.where(row==1)[0][0]
+        i2 = i1
+        #print("i1:", i1, " i2:", i2)
+        coinMat[i1][i2] += 1
+    if row.sum() == 2:
+        i1, i2 = numpy.where(row==1)[0]
+        #print("i1:", i1, " i2:", i2)
+        coinMat[i1][i2] += 1
+        coinMat[i2][i1] += 1
+
+print("Original Matrix (cmat)")
+print(cmat)
+print("Coincidence Matrix")
+print(coinMat)
+
+pl.matshow(coinMat, fignum=1, cmap=pl.cm.gray)
 pl.show()
