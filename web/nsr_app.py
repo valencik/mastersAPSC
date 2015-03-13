4d5aefca57b3bed48fdc0f3a7c286154060cabd5
Andrew Valencik <andrew.valencik@gmail.com>
2015-03-12 23:53:19 -0300

    Added first real API example topauthors
    Uses both mandatory HTTP GET parameters and optional ones

--- web/nsr_app.py
+++ web/nsr_app.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from flask import Flask
+from flask import Flask, jsonify, request
 
 # Setup the connection to MongoDB and get the NSR collection
 client = MongoClient('localhost', 27017)
@@ -21,6 +21,25 @@ def find_year(year_id):
     first_doc = nsr.find_one({"year": year_id})
     title = first_doc['title']
     return "The first title in {year} is: {title}".format(year=year_id, title=title)
+
+# API: topauthors
+# returns an array of authors in given year sorted by publication number
+@app.route('/api/topauthors')
+def aggregate():
+    topauthors_params = request.args
+    year_id = int(topauthors_params['year'])
+    limit_id = 2
+    if 'limit' in topauthors_params:
+        limit_id = int(topauthors_params['limit'])
+    topauthors_pipeline = [
+        {"$match": {"year": year_id}},
+        {"$unwind": "$authors"},
+        {"$group": {"_id": "$authors", "total": {"$sum": 1}}},
+        {"$sort": {"total": -1} },
+        {"$limit": limit_id}
+    ]
+    results = jsonify(nsr.aggregate(topauthors_pipeline))
+    return results
 
 
 # If executed directly from python interpreter, run local server
