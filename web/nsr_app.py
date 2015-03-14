f5b3cabab6eb7a05dc727d1cb79a03a41bc7e1c4
Andrew Valencik <andrew.valencik@gmail.com>
2015-03-13 22:32:05 -0300

    Enabled CORS

--- web/nsr_app.py
+++ web/nsr_app.py
@@ -12,6 +12,17 @@ nsr = db.NSR
 # Create our instance of the Flask class
 app = Flask(__name__)
 
+# Enable CORS
+# https://gist.github.com/blixt/54d0a8bf9f64ce2ec6b8
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response
+app.after_request(add_cors_headers)
 
 # Root site route
 @app.route('/')
