51573a2e929ea3b4169a33a86c028f9ed2222290
Andrew Valencik <andrew.valencik@gmail.com>
2015-04-04 23:57:25 -0300

    Added selector splitting

--- data/massageJSONtoSchema.pl
+++ data/massageJSONtoSchema.pl
@@ -10,7 +10,14 @@ s/^"_id":"((\d\d\d\d).*)",$/"_id":"$1",\n"year":$2,/g;
 s/^"code":"((\w*).*)",$/"code":"$1",\n"type":"$2",/g;
 
 # Turn author names into array elements
-s/[^]], /", "/g if /^"authors":\[/g;
+s/[^]], /", "/g if /^"authors":\[/;
+s/", "Jr."/, Jr."/g if /^"authors":\[/; #Fix the Jr's
 
 # Turn history stamps into array elements
-s/ /", "/g if /^"history":\[/g;
+s/ /", "/g if /^"history":\[/;
+
+# Split selectors
+#    "T:241AM;A. R:(N,2N);A. N:240AM;A. 
+s/([A-Z]):(.*?);(.*?)\./{"type":"$1", "value":"$2", "subkey":"$3"},/g if /^"selectors":\[/;
+s/^"selectors":\["/"selectors":\[/g;
+s/},"],$/}],/ if /^"selectors":\[/;
