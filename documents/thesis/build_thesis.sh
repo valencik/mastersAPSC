2bde3cda8634ce768918f1a74a4e026f67b9e715
Andrew Valencik <andrew.valencik@gmail.com>
2015-06-28 16:44:10 -0300

    Added table of contents and numbered sections

--- documents/thesis/build_thesis.sh
+++ documents/thesis/build_thesis.sh
@@ -3,6 +3,6 @@
 #Temporary comment filter for custom markdown
 sed '/^%- .*$/d' thesis.md > thesis_no_comments.md
 
-pandoc thesis_no_comments.md -o Thesis.pdf
+pandoc --table-of-contents --number-sections thesis_no_comments.md --output Thesis.pdf
 
 open Thesis.pdf
