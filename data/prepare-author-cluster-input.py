512cf34b1b895e67310a55d2b3aaa29c57885e5c
Andrew Valencik <andrew.valencik@gmail.com>
2015-07-11 14:01:11 -0300

    Create authorSummary collection if not exist

--- data/prepare-author-cluster-input.py
+++ data/prepare-author-cluster-input.py
@@ -1,16 +1,33 @@
+import sys
 from pymongo import MongoClient
 import csv
 
+# Connect to the main database
 client = MongoClient('localhost', 27017)
 db = client.masters
-nsr = db.newAuthorSummary
+collections = db.collection_names()
+if not 'NSR' in collections:
+    sys.exit("ERROR: NSR collection not found.")
 
-#db.newAuthorSummary.aggregate([{$project: {_id: 1, numCoauthors: {$size: "$coauthors"}, numYears: {$size: "$years"}, numEntries: {$size: "$papers"}}}])
-selectorAuthors_pipeline = [
-    {"$project": {"_id": 1, "numCoauthors": {"$size": "$coauthors"}, "numYears": {"$size": "$years"}, "numEntries": {"$size": "$papers"}}}
-]
-results = nsr.aggregate(selectorAuthors_pipeline, allowDiskUse=True)
+#Create the authorSummary collection
+if not 'authorSummary' in db.collection_names():
+    db.NSR.aggregate([
+        {"$project": {"_id": 1, "copyauthors": "$authors", "authors": 1, "year": 1, "selectors": 1}},
+        {"$unwind": "$authors"},
+        {"$unwind": "$copyauthors"},
+        {"$group": {"_id": "$authors", "coauthors": {"$addToSet": "$copyauthors"}, "years": {"$addToSet": "$year"},
+            "papers": {"$addToSet": "$_id"}}},
+        {"$out": "authorSummary"}
+    ], allowDiskUse=True)
 
+# Flatten the authorSummary data for clustering
+authorSummary_pipeline = [
+    {"$project": {"_id": 1, "numCoauthors": {"$size": "$coauthors"}, "numYears": {"$size": "$years"},
+        "numEntries": {"$size": "$papers"}}}
+    ]
+results = db.authorSummary.aggregate(authorSummary_pipeline, allowDiskUse=True)
+
+# Write the flattened data to a tsv file
 with open('author-cluster-input.tsv', 'w', newline='') as tsvfile:
     transaction_writer = csv.writer(tsvfile, delimiter='\t')
     for document in results:
