6d3be37280b2da249a24a4125ae3932801caac52
Andrew Valencik <andrew.valencik@gmail.com>
2015-07-12 21:30:30 -0300

    Removed old vim, sed, and bash code
    prepare-data.py and perl scripts now handles all of this.

--- data/create-NSR-database.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash -e
-
-cp NSRDump.out NSR.json
-perl parseNSRtoJSON.pl NSR.json
-perl massageJSONtoSchema.pl NSR.json
-perl flattenJSONforMongo.pl NSR.json
-
-mongoimport -d masters -c NSR --type json --file NSR.json
