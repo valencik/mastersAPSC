9a332cd72527ef7f66b582cbcad5099817ae9690
Andrew Valencik <andrew.valencik@gmail.com>
2015-07-18 18:13:27 -0300

    PEP8 formatting (used autopep8)

--- data/prepare-data.py
+++ data/prepare-data.py
@@ -4,14 +4,14 @@ import pymongo
 import subprocess
 import sys
 import statistics
-
-## {{{ http://code.activestate.com/recipes/511478/ (r1)
 import math
 import functools
 
-def percentile(N, percent, key=lambda x:x):
+
+def percentile(N, percent, key=lambda x: x):
     """
     Find the percentile of a list of values.
+    From: http://code.activestate.com/recipes/511478/
 
     @parameter N - is a list of values. Note N MUST BE already sorted.
     @parameter percent - a float value from 0.0 to 1.0.
@@ -21,21 +21,20 @@ def percentile(N, percent, key=lambda x:x):
     """
     if not N:
         return None
-    k = (len(N)-1) * percent
+    k = (len(N) - 1) * percent
     f = math.floor(k)
     c = math.ceil(k)
     if f == c:
         return key(N[int(k)])
-    d0 = key(N[int(f)]) * (c-k)
-    d1 = key(N[int(c)]) * (k-f)
-    return d0+d1
-## end of http://code.activestate.com/recipes/511478/ }}}
+    d0 = key(N[int(f)]) * (c - k)
+    d1 = key(N[int(c)]) * (k - f)
+    return d0 + d1
 
 
 # Connect to the local Mongo server
 try:
     client = pymongo.MongoClient('localhost', 27017, serverSelectionTimeoutMS=100)
-    client.admin.command('ismaster') # Test command to see if we can connect
+    client.admin.command('ismaster')  # Test command to see if we can connect
 
     # If we can conenct, reset client to defaults
     print("Successfully connected to MongoDB...")
@@ -44,14 +43,14 @@ except pymongo.errors.ConnectionFailure:
     sys.exit("ERROR: Could not connect to database, are you sure 'mongod' is running?")
 
 # Create 'masters' database
-if not 'masters' in client.database_names():
+if 'masters' not in client.database_names():
     print("Database 'masters' was not found, creating...")
 else:
     print("Found database 'masters'...")
 db = client['masters']
 
 # Import NSR data
-if not 'NSR' in db.collection_names():
+if 'NSR' not in db.collection_names():
     if not os.path.isfile("NSR.json"):
         if os.path.isfile("mastersNSRDataDump.tbz2"):
             print("Could not find database file NSR.json...")
@@ -71,26 +70,26 @@ if not 'NSR' in db.collection_names():
 print("Found NSR collection...")
 
 # Create collection authorSummary
-if not 'authorSummary' in db.collection_names():
+if 'authorSummary' not in db.collection_names():
     print("Collection authorSummary not found, creating...")
     db.NSR.aggregate([
         {"$project": {"_id": 1, "copyauthors": "$authors", "authors": 1, "year": 1}},
         {"$unwind": "$authors"},
         {"$unwind": "$copyauthors"},
-        {"$group": {"_id": "$authors", "coauthors": {"$addToSet": "$copyauthors"}, "years": {"$addToSet": "$year"},
-            "papers": {"$addToSet": "$_id"}}},
+        {"$group": {"_id": "$authors", "coauthors": {"$addToSet": "$copyauthors"},
+                    "years": {"$addToSet": "$year"}, "papers": {"$addToSet": "$_id"}}},
         {"$out": "authorSummary"}
     ], allowDiskUse=True)
 
 # Create collection authorSummaryByYear
-if not 'authorSummaryByYear' in db.collection_names():
+if 'authorSummaryByYear' not in db.collection_names():
     print("Collection authorSummaryByYear not found, creating...")
     db.NSR.aggregate([
         {"$project": {"_id": 1, "copyauthors": "$authors", "authors": 1, "year": 1}},
         {"$unwind": "$authors"},
         {"$unwind": "$copyauthors"},
-        {"$group": {"_id": {"author": "$authors", "year": "$year"}, "coauthors": {"$addToSet": "$copyauthors"},
-            "papers": {"$addToSet": "$_id"}}},
+        {"$group": {"_id": {"author": "$authors", "year": "$year"}, "coauthors": {
+            "$addToSet": "$copyauthors"}, "papers": {"$addToSet": "$_id"}}},
         {"$out": "authorSummaryByYear"}
     ], allowDiskUse=True)
 
@@ -100,7 +99,7 @@ print("Aggregating authorSummary data...")
 authorSummary_pipeline = [
     {"$project": {"_id": 0, "author": "$_id", "numCoauthors": {"$size": "$coauthors"},
                   "numYears": {"$size": "$years"}, "numEntries": {"$size": "$papers"}}}
-    ]
+]
 results = db.authorSummary.aggregate(authorSummary_pipeline, allowDiskUse=True)
 with open('author-cluster-input.tsv', 'w', newline='') as tsvfile:
     print("Writing authorSummary data to file...")
@@ -117,12 +116,11 @@ with open('author-cluster-input.tsv', 'w', newline='') as tsvfile:
 # Generate authorSummaryByYear tsv for clustering
 print("Aggregating authorSummaryByYear data...")
 authorSummaryByYear_pipeline = [
-    {"$group": {"_id": "$_id.author", "yearData":
-        {"$push": {"year": "$_id.year", "numCoauthors": {"$size": "$coauthors"}, "numEntries": {"$size": "$papers"}}}
-    }},
+    {"$group": {"_id": "$_id.author", "yearData": {
+        "$push": {"year": "$_id.year", "numCoauthors": {"$size": "$coauthors"}, "numEntries": {"$size": "$papers"}}}}},
     {"$project": {"author": "$_id", "yearData": 1, "numYears": {"$size": "$yearData"}}},
     {"$match": {"numYears": {"$gt": 3}}}
-    ]
+]
 results = db.authorSummaryByYear.aggregate(authorSummaryByYear_pipeline, allowDiskUse=True)
 with open('author-cluster-entry-quartiles-input.tsv', 'w', newline='') as tsvfile:
     print("Writing authorSummaryByYear data to file...")
@@ -141,7 +139,7 @@ with open('author-cluster-entry-quartiles-input.tsv', 'w', newline='') as tsvfil
         sumEntries = entries.copy()
         for i, entry in enumerate(sumEntries):
             if i >= 1:
-                sumEntries[i] = sumEntries[i] + sumEntries[i-1]
+                sumEntries[i] = sumEntries[i] + sumEntries[i - 1]
         numEntries025 = math.floor(percentile(sumEntries, 0.25))
         numEntries050 = math.floor(percentile(sumEntries, 0.50))
         numEntries075 = math.floor(percentile(sumEntries, 0.75))
@@ -158,7 +156,7 @@ with open('author-cluster-entry-quartiles-input.tsv', 'w', newline='') as tsvfil
 # Generate transactions tsv for association rule learning
 print("Aggregating transaction data...")
 selectorAuthors_pipeline = [
-    {"$match": {"selectors.type":"N"}},
+    {"$match": {"selectors.type": "N"}},
     {"$unwind": "$selectors"},
     {"$unwind": "$authors"},
     {"$group": {"_id": "$selectors.value", "authors": {"$addToSet": "$authors"}}}
